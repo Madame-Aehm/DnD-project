a80b34876625b2d964daf84e0226aebc47c1afa1
Emily <emilyrachelstickler@gmail.com>
2022-09-05 18:02:16 +0200

    few extra views

--- src/views/Languages.js
+++ src/views/Languages.js
@@ -1,11 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Loader from '../components/Loader';
 import NavBar from '../components/NavBar'
 
 function Languages() {
+
+    const [languagesList, setLanguagesList] = useState([]);
+    const [language, setLanguage] = useState({})
+    const [pageLoader, setPageLoader] = useState(true);
+    const [loader, setLoader] = useState(true);
+    const [error, setError] = useState(null);
+
+    const fetchList = async() => {
+        if (languagesList.length === 0) {
+          try {
+            const response = await fetch("https://www.dnd5eapi.co/api/languages");
+            const result = await response.json();
+            setLanguagesList(result.results);
+            console.log(result)
+            setTimeout(() => {
+              setPageLoader(false);
+            }, 1000);
+          } catch (error) {
+            console.log("error", error)
+            setError(error);
+          }
+        }
+      }
+  
+    useEffect(() => {
+        fetchList();
+    }, []);
+
+    async function scoreFetch(restURL) {
+        try {
+            const response = await fetch(`https://www.dnd5eapi.co${restURL}`);
+            const result = await response.json();
+            setLanguage(result);
+            setLoader(false);
+            console.log(result);
+        } catch (error) {
+            console.log("error", error)
+            setError(error);
+        }
+      }
+
   return (
     <div className='content-container'>
         <NavBar/>
         <h1>Languages</h1>
+        {error && <p>Something went wrong.. Please reload.</p>}
+        {pageLoader && <Loader/>}
+        {!pageLoader && 
+            <>
+                <div className='checkbox-container-2'>
+                    {languagesList.map((item) => {
+                    return (
+                        <div className='larger-checkbox' key={item.index}>
+                            <input type={"radio"} 
+                                name={"conditions"} 
+                                value={item.url} id={item.index} 
+                                onChange={
+                                (e) => {
+                                    setLoader(true);
+                                    scoreFetch(item.url)
+                                }
+                                }/>
+                            <label htmlFor={item.index}>{item.name}</label>
+                        </div>
+                    )
+                    })}
+                </div>
+                {loader && <p>loading...</p>}
+                {!loader &&
+                    <>
+                        <div className='display'>
+                            <h3>{language.name}</h3>
+                            <p>Script: <b>{language.script}</b> | Type: <b>{language.type}</b></p>
+                        <hr/>
+                            <h5>Typical Speakers</h5>
+                            <div className='mini-h-list'>
+                                {language.typical_speakers.map((item, i) => {
+                                    return <h6 key={i}>{item}</h6>
+                                })}
+                            </div>
+                        <p></p>
+                        </div>
+                    </>
+                }
+            </>
+        }
     </div>
   )
 }
